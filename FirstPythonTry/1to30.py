302931824257e4476180d7dd87a4cf6bde16ea19
Punhan Panahov <punhanpanahov8@gmail.com>
2022-03-25 16:56:03 +0400

    number task

--- FirstPythonTry/1to30.py
+++ FirstPythonTry/1to30.py
@@ -2,5 +2,5 @@ def printno(upper):
     if(upper>0):
         printno(upper-1)
         print(upper)
-upper=int(input("Enter upper limit: "))
+upper=int(input("Upper,ust heddi yaz: "))
 printno(upper)
